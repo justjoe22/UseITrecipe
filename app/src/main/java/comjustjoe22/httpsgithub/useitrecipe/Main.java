e4ebe798b7d611faade5a1265ae33a515929cc3e
Joe Prewitt <justjoe22@gmail.com>
2016-05-17 14:49:50 -0400

    Added Firebase to the Android Studio app.

--- app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
+++ app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
@@ -3,11 +3,18 @@ package comjustjoe22.httpsgithub.useitrecipe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
+
+        Firebase myDBRef = new Firebase("https://useitrecipe.firebaseio.com/");
+
+        myDBRef.child("message").setValue("Do you have data? You'll love Firebase.");
     }
 }
