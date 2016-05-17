1373db817d47949d7d2e6dc082b74a2028a5e323
Joe Prewitt <justjoe22@gmail.com>
2016-05-17 15:23:24 -0400

    Added Firebase to the Android Studio app.

--- app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
+++ app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
@@ -3,7 +3,10 @@ package comjustjoe22.httpsgithub.useitrecipe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.ValueEventListener;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class Main extends AppCompatActivity {
 
