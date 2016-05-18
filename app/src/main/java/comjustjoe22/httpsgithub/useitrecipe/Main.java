e1b8c52de7efb09bad2600764ad5d6e6ebae6ce4
Joe Prewitt <justjoe22@gmail.com>
2016-05-18 15:47:02 -0400

    Populating ListView

--- app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
+++ app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
@@ -1,14 +1,27 @@
 package comjustjoe22.httpsgithub.useitrecipe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.ValueEventListener;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
-public class Main extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Main extends ListActivity  {
+
+    // TODO: change this to your own Firebase URL
+    private static final String FIREBASE_URL = "https://useitrecipe.firebaseio.com/";
+
+    //private String mUsername;
+    //private Firebase mFirebaseRef;
+    //private ValueEventListener mConnectedListener;
+    //private ListView userInfoList;
+    //private ArrayList fbItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +29,30 @@ public class Main extends AppCompatActivity {
         Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
 
-        Firebase myDBRef = new Firebase("https://useitrecipe.firebaseio.com/");
+        Firebase mFirebaseRef = new Firebase(FIREBASE_URL).child("message");
 
-        myDBRef.child("message").setValue("Do you have data? You'll love Firebase.");
+        mFirebaseRef.setValue("Do you have data? You'll love Firebase.");
 
-        myDBRef.child("message").addValueEventListener(new ValueEventListener() {
+        final ArrayList fbItems = new ArrayList();
 
+        mFirebaseRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
             public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+                fbItems.add(snapshot.getValue());
             }
-
-            public void onCancelled(FirebaseError error) { }
-
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                //String message = "Server error. Refresh page";
+            }
         });
+
+        ArrayAdapter itemsAdapter =
+                new ArrayAdapter (this, android.R.layout.simple_list_item_1, fbItems);
+
+        ListView listView = (ListView) findViewById(R.id.firebaseView);
+        listView.setAdapter(itemsAdapter);
     }
+
+
 }
