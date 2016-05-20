edcf78f96569323dc182fd4e297b03cc74578318
Joe Prewitt <justjoe22@gmail.com>
2016-05-20 15:58:58 -0400

    ListView Adapter Changes

--- app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
+++ app/src/main/java/comjustjoe22/httpsgithub/useitrecipe/Main.java
@@ -1,10 +1,10 @@
 package comjustjoe22.httpsgithub.useitrecipe;
 
-import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+//import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.firebase.client.DataSnapshot;
@@ -14,13 +14,15 @@ import com.firebase.client.FirebaseError;
 
 import java.util.ArrayList;
 
-public class Main extends ListActivity  {
+public class Main extends AppCompatActivity   {
 
     private static final String frBASE_URL = "https://useitrecipe.firebaseio.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
 
         //Place Firebase code here
         Firebase.setAndroidContext(this);
@@ -29,16 +31,15 @@ public class Main extends ListActivity  {
 
         final ArrayList fbItems = new ArrayList();
 
-        final ListAdapter itemsAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, fbItems);
+        final ListView listView = (ListView) findViewById(R.id.fbList);
+
+        //final ListAdapter itemsAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, fbItems);
 
         mFrBaseRef.addListenerForSingleValueEvent(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot snapshot) {
                         fbItems.add(snapshot.getValue());
-
-                        ListView listView = (ListView) findViewById(R.id.fbList);
-                        listView.setAdapter(itemsAdapter);
 
                         Log.d("DATA", snapshot.getValue().toString());
                     }
@@ -49,6 +50,12 @@ public class Main extends ListActivity  {
                     }
 
                 });
+
+        if(listView!=null)
+        {
+            listView.setAdapter(new ArrayAdapter(Main.this,android.R.layout.simple_list_item_1,fbItems));
+            listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        }
 
         //mFrBaseRef.child("Note3").setValue("Do you have data? You'll love Firebase.");
 
